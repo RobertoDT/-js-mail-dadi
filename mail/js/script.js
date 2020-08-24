366c6634e1bc1efdcc47e8d797f8af861046bfb9
Roberto Del Toro <robertodeltoro93@gmail.com>
2020-08-24 16:04:16 +0200

    Esercizio Email Validator

---
+++ mail/js/script.js
@@ -0,0 +1,23 @@
+// Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere,
+// stampa un messaggio appropriato sull’esito del controllo.
+
+var elenco = [
+  "pippo@pluto.com",
+  "robertodeltoro93@gmail.com",
+  "nonsocosascrivere@hotmail.it",
+  "ciao@mai.ir",
+  "pluto@pippo.com",
+  "enzo.pierga@libero.eu"
+]
+
+var email = prompt("Inserisci la tua email");
+
+for(var i=0; i < elenco.length; i++){
+  if(email == elenco[i]){
+    alert("Sei nella lista degli invitati, Benvenuto!");
+    break;
+  }
+}
+if(email != elenco[i]){
+  alert("Mi dispiace, non sei nella lista!");
+}
